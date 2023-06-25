ccddf5118982e1eb67f177153f7d0c7c139f6fb1
ylitchan <898475174@qq.com>
2023-06-25 20:10:58 +0800

    列表member放入bloom

--- pyDemo/alphaDS/utils/SBF.py
+++ pyDemo/alphaDS/utils/SBF.py
@@ -2,26 +2,33 @@ from tools import *
 
 
 def get_list_members(ids: list):
-    already = []
+    sbfilter = ScalableBloomFilter(mode=ScalableBloomFilter.SMALL_SET_GROWTH)
+    # already = []
     for list_id in ids:
         member = client_tweet.get_list_members(list_id, pagination_token=None, user_auth=True)
         next_token = member.meta.get('next_token')
-        already.extend([i.id for i in member.data])
-        print('列表数量', len(already))
+        for i in member.data:
+            sbfilter.add(i.id)
+        # already.extend([i.id for i in member.data])
+        # print('列表数量', len(already))
         while next_token:
             try:
                 member = client_tweet.get_list_members(list_id, pagination_token=next_token)
                 next_token = member.meta.get('next_token')
-                already.extend([i.id for i in member.data])
-                print('获取成功', len(already))
+                for i in member.data:
+                    sbfilter.add(i.id)
+                # already.extend([i.id for i in member.data])
+                # print('获取成功', len(already))
                 time.sleep(60)
             except:
                 print('获取错误')
                 time.sleep(900)
                 continue
         time.sleep(60)
-    with open('listMembers.json', 'w') as f:
-        json.dump(list(set(already)), f)
+    # with open('listMembers.json', 'w') as f:
+    #     json.dump(list(set(already)), f)
+    with open('sbfilterMember', 'wb') as f:
+        sbfilter.tofile(f)
 
 
 if __name__ == '__main__':
